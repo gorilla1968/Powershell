0b942acc30e5a4d589df051faaae865725aa48b0
gorilla1968 <58355212+gorilla1968@users.noreply.github.com>
2025-01-29 11:03:28 +0100

    Update remeditation.ps1

--- .Net3.5/remeditation.ps1
+++ .Net3.5/remeditation.ps1
@@ -1,1 +1,12 @@
-
+try  {
+ 
+    Enable-WindowsOptionalFeature -Online -FeatureName NetFx3
+    Write-Output 'NetFx3 will be Enabled'
+    exit 0
+}
+catch {
+ 
+    $errMsg = $_.Exception.Message
+    Write-host $errMsg
+    exit 1
+}
